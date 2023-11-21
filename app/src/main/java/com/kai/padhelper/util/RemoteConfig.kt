fe74531db74777180076e9d5584bfd54662167c0
loyalgenerous <loyalgenerous@hotmail.com>
2023-11-21 15:51:43 +0800

    Revert "Updating to KSP instead KAPT"
    This reverts commit 53ed9bef65b4afaa73b8562d010f805d15150467.

--- app/src/main/java/com/kai/padhelper/util/RemoteConfig.kt
+++ app/src/main/java/com/kai/padhelper/util/RemoteConfig.kt
@@ -24,6 +24,7 @@ object RemoteConfig {
             if (it.isSuccessful) {
                 MAX_CHARACTER_INDEX = firebaseRemoteConfig.getLong("max_character_index")
             }
+            // 可以在这里通知其他部分配置已更新
         }
 
         initialized = true
